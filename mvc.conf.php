86e3f277ab5a28dc302e768bf543557abf850903
Bosun Egberinde <bosunski@gmail.com>
2018-03-06 10:03:57 +0100

    gitignored some files

--- mvc.conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//error_reporting(0);
-	define('DB', 'mvc');
-	define('DBHOST', 'localhost');
-	define('DBUNAME', 'root');
-	define('DBPASS', 'gabriel10');
-	define('ABSPATH', dirname(__FILE__));
-	define('HOME', '');
-	define('AUTH_KEY', 'PAPAsukataLebanonx5x5');
